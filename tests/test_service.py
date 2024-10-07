c15bfea5c2cc53db0369c07a49e893254d8cf19b
Bander <46300268+xZetsubou@users.noreply.github.com>
2024-10-07 07:01:27 +0300

    release: v3.17.0

--- tests/test_service.py
+++ tests/test_service.py
@@ -20,10 +20,10 @@ def __mock_monitor(api):
     return restore
 
 def __mock_is_playingvideo(api, mock_state):
-    default = api.core.kodi.xbmc.player().isPlayingVideo
-    api.core.kodi.xbmc.player().isPlayingVideo = mock_state
+    default = api.core.kodi.xbmc.playere().isPlayingVideo
+    api.core.kodi.xbmc.playere().isPlayingVideo = lambda: mock_state
     def restore():
-        api.core.kodi.xbmc.player().isPlayingVideo = default
+        api.core.kodi.xbmc.playere().isPlayingVideo = default
     return restore
 
 def __mock_get_cond_visibility(api, mock_data):
